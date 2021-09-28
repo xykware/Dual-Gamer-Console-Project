3f21d404ba6189866191bfd5d7a20af29195b7b0
Ethan Richardson <ethan.taylor.richardson@gmail.com>
2021-09-28 15:40:38 -0400

    Add project files.

---
+++ Dual Gamer Console Project/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dual_Gamer_Console_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
